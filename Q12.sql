0d6beb7c431a3a4e7af053c811cd3ef8e2adb75e
crazyArman786 <armanbharadwaj7@gmail.com>
2026-07-29 19:35:48 +0530

    Add files via upload

---
+++ Q12.sql
@@ -0,0 +1,25 @@
+-- Table: Employee
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+ 
+
+-- Write a solution to find managers with at least five direct reports.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+
+SELECT m.name FROM Employee e JOIN Employee m ON e.managerId = m.id GROUP BY m.id, m.name HAVING 
+COUNT(e.id) >= 5;
